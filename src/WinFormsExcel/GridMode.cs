a2f63565551941150d083cae68819f0a40b12282
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-10-01 13:13:11 +0500

    Add XML comments for GridMode

--- src/WinFormsExcel/GridMode.cs
+++ src/WinFormsExcel/GridMode.cs
@@ -1,18 +1,35 @@
 ﻿/* WinForms Excel library 
- * Copyright (c) 2020,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 3-Clause */
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace ExtraControls
 {
+    /// <summary>
+    /// Represents mode in which C_AdvancedDataGrid control operates
+    /// </summary>
     public enum GridMode
     {
-        Undefined = 0, //not initialized yet
-        Auto = 1, //Choose mode automatically: Excel mode if possible, otherwise Substitute
-        Excel = 2, //Uses AdvancedDataGrid
-        Substitute =3 //Uses S_AdvancedDataGrid
+        /// <summary>
+        /// Undefined (control is not initialized yet)
+        /// </summary>
+        Undefined = 0,
+        
+        /// <summary>
+        /// Choose mode automatically: Excel mode if possible, otherwise Substitute
+        /// </summary>
+        Auto = 1,
+        
+        /// <summary>
+        /// Uses Excel implementation (AdvancedDataGrid)
+        /// </summary>        
+        Excel = 2,
+        
+        /// <summary>
+        /// Uses substitited implementation (S_AdvancedDataGrid)
+        /// </summary>
+        Substitute =3
     }
 }
